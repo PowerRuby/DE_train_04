90724bfe5d2a309d58974b4555e3845c44651675
Andrea Ribuoli <andrea@powerruby.com>
2018-11-04 10:12:17 +1345

    refine app/jobs/clever_utility_job.rb

--- v02/app/jobs/clever_utility_job.rb
+++ v02/app/jobs/clever_utility_job.rb
@@ -2,6 +2,7 @@ class CleverUtilityJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+    sleep 15
+    logger.info "Logging this message after a delay of 15 seconds."
   end
 end
